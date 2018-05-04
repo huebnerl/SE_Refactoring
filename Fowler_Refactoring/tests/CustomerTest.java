5e14af0699100262a7130b7ec5386a87cbf19eb7
huebnerl <LarsHuebner2@web.de>
2018-05-04 15:48:30 +0200

    Pretty printing of code.

---
+++ Fowler_Refactoring/tests/CustomerTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by D067836 on 04.05.2018.
+ */
+class CustomerTest {
+    @Test
+    void addRental() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void statement() {
+
+    }
+
+}
