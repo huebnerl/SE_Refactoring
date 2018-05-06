a61ce71ffeffd0ed229f5ad0783cac0af3585270
huebnerl <LarsHuebner2@web.de>
2018-05-06 11:13:05 +0200

    Moved getFrequentRenterPoints() to Movie class.

--- Fowler_Refactoring/src/Rental.java
+++ Fowler_Refactoring/src/Rental.java
@@ -20,14 +20,6 @@ class Rental {
     }
 
     public int getFrequentRenterPoints(){
-        int frequentRenterPoints = 0;
-
-        // add frequent renter points
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            frequentRenterPoints++;
-
-        return frequentRenterPoints;
+        return getMovie().getFrequentRenterPoints(daysRented);
     }
 }
