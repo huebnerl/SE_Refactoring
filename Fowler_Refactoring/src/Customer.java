03c28e543431e27d14091dec3a88dc99f4a5669f
huebnerl <LarsHuebner2@web.de>
2018-05-06 09:43:19 +0200

    Removed redundant not changing local variable double thisAmount.

--- Fowler_Refactoring/src/Customer.java
+++ Fowler_Refactoring/src/Customer.java
@@ -24,18 +24,15 @@ class Customer {
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = each.getPrice();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getPrice()) + "\n";
+            totalAmount += each.getPrice();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
