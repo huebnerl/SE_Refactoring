78c29ba2f3affeebe7313dcec751cc8a4e27d9b7
huebnerl <LarsHuebner2@web.de>
2018-05-06 11:44:24 +0200

    Moved getPrice() to Price class and rename it to getFee().

--- Fowler_Refactoring/src/Movie.java
+++ Fowler_Refactoring/src/Movie.java
@@ -34,26 +34,8 @@ public class Movie {
         return title;
     }
 
-   public double getPrice(int daysRented){
-       double result = 0;
-       switch (getPriceCode()) {
-           case Movie.REGULAR:
-               result += 2;
-               if (daysRented > 2)
-                   result += (daysRented - 2) * 1.5;
-               break;
-           case Movie.NEW_RELEASE:
-               result += daysRented * 3;
-               break;
-           case Movie.CHILDREN:
-               result += 1.5;
-               if (daysRented > 3)
-                   result += (daysRented - 3) * 1.5;
-               break;
-           default:
-               break;
-       }
-       return result;
+   public double getFee(int daysRented){
+        return price.getFee(daysRented);
    }
 
     public int getFrequentRenterPoints(int daysRented){
